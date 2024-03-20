798be4e6a48dd506cb7e8de7f7034a440cbf263a
Nyx <lilizhao107513@gmail.com>
2024-03-20 15:46:17 -0400

    init

---
+++ app/src/main/java/com/example/lilinyxzhao_horchatas_sharedpreferencesapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lilinyxzhao_horchatas_sharedpreferencesapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
